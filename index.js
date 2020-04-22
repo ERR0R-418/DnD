a4901e0f13c62862117b06adfcf3fd2bc655205e
ERR0R-418 <k.kiohai@gmail.com>
2020-04-22 18:50:03 +0300

    inithbs

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-let myFile = require('./main.json');
+const hbsm = require('express-handlebars');
+const express = require('express')
+const fs = require('fs')
+const app = express()
+let router = express.Router();
+const port = 3000
+
+app.engine('handlebars', hbsm());
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res, next) {
+  console.log(req)
+  res.render('home');
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
