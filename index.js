fa6cff79383cfa3a6c98c8f38952fae3edddd5ad
VaskillerDev <Vaskoooo9241@gmail.com>
2020-04-22 20:08:48 +0300

    add dataStorage and getDataStorage function

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const hbsm = require('express-handlebars');
 const express = require('express')
 const fs = require('fs')
 const app = express()
-let router = express.Router();
 const port = 3000
 
 app.engine('handlebars', hbsm());
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res, next) {
-  console.log(req)
-  res.render('home');
+const getDataStorage = (fileName) => {
+  // read file
+  const readDataStorage = () => {
+    let file = fs.readFileSync(`${__dirname}/${fileName}`).toString()
+    return JSON.parse(file);
+  };
+  let questions = readDataStorage();
+  // json -> array
+  let accum =[];
+  for (let question in questions)
+    if (questions.hasOwnProperty(question))
+      accum.push(questions[question])
+  return accum;
+};
+
+const dataStorage = {question: getDataStorage("dataStorage.json")}
+
+app.get('/', function(req, res) {
+  res.render('home',{questions: dataStorage.question});
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
