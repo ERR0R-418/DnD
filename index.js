06ed2eb9e804be2d3774641d726b6aba3bf768b1
VaskillerDev <Vaskoooo9241@gmail.com>
2020-05-02 21:55:55 +0300

    init

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-const hbsm = require('express-handlebars');
+const DataFromDatabase = require ("./src/DataFromDatabase");
+const expressHandlebars = require('express-handlebars')
 const express = require('express')
 const fs = require('fs')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const urlParser = bodyParser.urlencoded({ extended: false })
 
-app.engine('handlebars', hbsm());
-app.set('view engine', 'handlebars');
+app.engine('handlebars', expressHandlebars())
+app.set('view engine', 'handlebars')
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 const getDataStorage = (fileName) => {
-  // read file
-  const readDataStorage = () => {
-    let file = fs.readFileSync(`${__dirname}/${fileName}`).toString()
-    return JSON.parse(file);
-  };
-  let questions = readDataStorage();
-  // json -> array
-  let accum =[];
-  for (let question in questions)
-    if (questions.hasOwnProperty(question))
-      accum.push(questions[question])
-  return accum;
-};
-
-const dataStorage = {question: getDataStorage("dataStorage.json")}
-
-/* get all answer-group
-let acc = new Set();
-let a = dataStorage.question.forEach((e)=>{
-  acc.add(e['answer-group'])
+    // read file
+    const readDataStorage = () => {
+        let file = fs.readFileSync(`${__dirname}/${fileName}`).toString()
+        return JSON.parse(file)
+    }
+    let questions = readDataStorage()
+    // json -> array
+    let accum = []
+    for (let question in questions)
+        if (questions.hasOwnProperty(question)) accum.push(questions[question])
+    return accum
 }
-console.log(acc)
-)*/
+
+const dataStorage = { question: getDataStorage('dataStorage.json') }
+
+const dataFromDatabase = DataFromDatabase();
 
 app.use(express.static('public'))
 
-app.use('/get_answers',(req,res)=>{
-  res.send(dataStorage)
+app.use('/send',(req,res)=>{
+    //console.log(req.body);
+    console.l
+    res.status(200).end();
 })
 
-app.use('/', function(req, res) {
-  res.render('home',{questions: dataStorage.question});
-});
+app.use('/get_answers', (req, res) => {
+    res.send(dataStorage)
+})
 
+app.use('/', function (req, res) {
+    res.render('home', { questions: dataStorage.question })
+})
 
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+)
